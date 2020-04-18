21f0697ffd8fc85c6798c90254b0151e55345a31
sehe <sgheeren@gmail.com>
2020-04-18 04:55:17 +0200

    Move semantics in AstGen (ew)
    What a mess. Instrumentation did not show improved perf, so I'd vote
    strike this idea

--- ast_gen.cpp
+++ ast_gen.cpp
@@ -2,10 +2,11 @@
 
 namespace AstGen {
 
-    Gen Gen::operator[](ListGen l) const {
-        return { Ast::Subscript { wrapped, l.elements } };
+    Gen Gen::operator[](ListGen l) && {
+        return { Ast::Subscript { std::move(wrapped), std::move(l.elements) } };
     }
 
-    Gen Gen::MemGen::operator[](ListGen l) const { return gen()[l]; }
+    Gen Gen::MemGen::operator[](ListGen l) && { return std::move(*this).gen()[std::move(l)]; }
+    Gen Gen::MemGen::operator[](ListGen l) const& { return gen()[std::move(l)]; }
 
 }
--- ast_gen.h
+++ ast_gen.h
@@ -16,61 +16,68 @@ namespace AstGen {
         Gen(unsigned long long v)      : wrapped(Ast::Number(v)) {}
         Gen(long double v)             : wrapped(Ast::Number(v)) {}
 
-        operator Ast::Expression const&() const { return wrapped; }
+        operator Ast::Expression()&& { return std::move(wrapped); }
+        operator Ast::Expression() const& { return wrapped; }
 
-        Gen operator=(Gen const& rhs) const { return Ast::Binary { wrapped, Gen(rhs), Operator::Assign }; }
+        Gen operator=(Gen rhs) && { return Ast::Binary { std::move(wrapped), std::move(rhs), Operator::Assign }; }
+        Gen operator=(Gen rhs) const& { return Ast::Binary { wrapped, std::move(rhs), Operator::Assign }; }
 
-        template <typename... T> Gen operator()(T const&... v) const {
-            return { Ast::Call { wrapped, {Gen(v)...} } };
+        template <typename... T> Gen operator()(T&&... v) && {
+            return { Ast::Call { wrapped, {Gen(std::forward<T>(v))...} } };
         }
-        Gen operator[](ListGen) const;
+        Gen operator[](ListGen) &&;
 
         struct MemGen {
             Ast::Expression obj;
             Ast::Identifier member;
 
-            Gen gen() const { return {Ast::Member {obj, member} }; }
-            operator Gen() const { return gen(); }
-            operator Ast::Expression() const { return gen(); }
-            template <typename... T> Gen operator()(T const&... v) const {
-                return gen()(v...);
+            Gen gen() && { return {Ast::Member {std::move(obj), std::move(member)} }; }
+            Gen gen() const& { return {Ast::Member {obj, member} }; }
+            operator Ast::Expression() && { return std::move(*this).gen(); }
+            operator Ast::Expression() const& { return gen(); }
+            template <typename... T> Gen operator()(T&&... v) && {
+                return std::move(*this).gen()(std::forward<T>(v)...);
             }
-            Gen operator[](ListGen) const;
+            template <typename... T> Gen operator()(T&&... v) const& {
+                return gen()(std::forward<T>(v)...);
+            }
+            Gen operator[](ListGen) &&;
+            Gen operator[](ListGen) const&;
         } const
             foo { wrapped, Ast::Identifier("foo") },
             bar { wrapped, Ast::Identifier("bar") },
             qux { wrapped, Ast::Identifier("qux") };
     };
 
-    static inline Gen operator+(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Plus }}; }
-    static inline Gen operator-(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Minus }}; }
-    static inline Gen operator*(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Mult }}; }
-    static inline Gen operator/(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Div }}; }
-    static inline Gen operator%(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Mod }}; }
+    static inline Gen operator+(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Plus }}; }
+    static inline Gen operator-(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Minus }}; }
+    static inline Gen operator*(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Mult }}; }
+    static inline Gen operator/(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Div }}; }
+    static inline Gen operator%(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Mod }}; }
 
-    static inline Gen operator< (Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Less }}; }
-    static inline Gen operator<=(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::LessEq }}; }
-    static inline Gen operator> (Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Greater }}; }
-    static inline Gen operator>=(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::GreaterEq }}; }
-    static inline Gen operator==(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::Equal }}; }
-    static inline Gen operator!=(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::NotEq }}; }
+    static inline Gen operator< (Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Less }}; }
+    static inline Gen operator<=(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::LessEq }}; }
+    static inline Gen operator> (Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Greater }}; }
+    static inline Gen operator>=(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::GreaterEq }}; }
+    static inline Gen operator==(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::Equal }}; }
+    static inline Gen operator!=(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::NotEq }}; }
 
-    static inline Gen operator& (Gen a, Gen b) { return {Ast::Binary { a, b, Operator::AND }}; }
-    static inline Gen operator| (Gen a, Gen b) { return {Ast::Binary { a, b, Operator::OR }}; }
-    static inline Gen operator^ (Gen a, Gen b) { return {Ast::Binary { a, b, Operator::XOR }}; }
-    static inline Gen operator&&(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::AND }}; }
-    static inline Gen operator||(Gen a, Gen b) { return {Ast::Binary { a, b, Operator::OR }}; }
+    static inline Gen operator& (Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::AND }}; }
+    static inline Gen operator| (Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::OR }}; }
+    static inline Gen operator^ (Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::XOR }}; }
+    static inline Gen operator&&(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::AND }}; }
+    static inline Gen operator||(Gen a, Gen b) { return {Ast::Binary { std::move(a), std::move(b), Operator::OR }}; }
 
-    static inline Gen operator+(Gen a) { return {Ast::Unary { Operator::UnaryPlus, a }}; }
-    static inline Gen operator-(Gen a) { return {Ast::Unary { Operator::UnaryMinus, a }}; }
-    static inline Gen operator!(Gen a) { return {Ast::Unary { Operator::NOT, a }}; }
+    static inline Gen operator+(Gen a) { return {Ast::Unary { Operator::UnaryPlus, std::move(a) }}; }
+    static inline Gen operator-(Gen a) { return {Ast::Unary { Operator::UnaryMinus, std::move(a) }}; }
+    static inline Gen operator!(Gen a) { return {Ast::Unary { Operator::NOT, std::move(a) }}; }
 
     struct ListGen {
         std::vector<Ast::Expression> elements;
-        ListGen(Gen g) : elements{g} {}
+        ListGen(Gen&& g) : elements{std::move(g)} {}
         ListGen(std::initializer_list<Ast::Expression> i) : elements(i) {}
     };
 
-    static inline ListGen operator,(Gen a, Gen b) { return { {a,b} }; }
-    static inline ListGen operator,(ListGen a, Gen b) { a.elements.push_back(b); return a; }
+    static inline ListGen operator,(Gen a, Gen b) { return { {std::move(a),std::move(b)} }; }
+    static inline ListGen operator,(ListGen a, Gen b) { a.elements.push_back(std::move(b)); return std::move(std::move(a)); }
 }
